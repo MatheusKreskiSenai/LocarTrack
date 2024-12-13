e34878275e2954e14aac26213f4d54b83bb5936c
Matheus Kreski <matheus.kreski@aluno.senai.br>
2024-12-13 17:22:05 -0300

    Adicionando as coisas da locadora

---
+++ LocarAndTrack.online/Services/LocacaoService.cs
@@ -0,0 +1,21 @@
+﻿using LocarAndTrack.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace LocarAndTrack.online.Services
+{
+    public class LocacaoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public LocacaoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Locacao>> ObterLocacoesAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Locacao>>("api/Locacoes");
+        }
+    }
+}
---
+++ LocarAndTrack.online/Services/LocadoraService.cs
@@ -0,0 +1,27 @@
+﻿using LocarAndTrack.Models;
+using System.Net.Http.Json;
+
+namespace LocarAndTrack.online.Services
+{
+    public class LocadoraService
+    {
+        private readonly HttpClient _httpClient;
+
+        public LocadoraService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Locadora>> ObterLocadorasAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Locadora>>("api/Locadoras");
+        }
+
+        public async Task<bool> PostLocadora(Locadora locadora)
+        {
+            var resposta = await _httpClient.PostAsJsonAsync("api/locadoras", locadora);
+
+            return resposta.IsSuccessStatusCode;
+        }
+    }
+}
